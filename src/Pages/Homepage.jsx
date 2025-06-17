6bceb9fd665d7191107bfaebc33c735870520780
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-17 22:17:06 +0200

    added meme

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,7 +1,14 @@
+import { Image } from "@chakra-ui/react";
 import React from "react";
+import meme from "../images/27w39b.jpg";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      Homepage
+      <Image src={meme} width={500} height={500} alt="meme" />
+    </div>
+  );
 };
 
 export default Homepage;
