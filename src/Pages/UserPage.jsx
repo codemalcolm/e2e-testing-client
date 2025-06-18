9b4bfab6f8b47649f43ad5d84507234740e337c5
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-18 16:01:48 +0200

    Added fetching user data when user visits UserPage

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { Box, Button, Flex, Spinner, Text } from "@chakra-ui/react";
+import useFetchUserProfile from "../hooks/useFetchUserProfile";
+
 const UserPage = () => {
-  return <div>UserPage</div>;
+  const { userData, loading, error } = useFetchUserProfile();
+
+  return (
+    <Flex justifyContent={"center"} alignItems={"center"} flexDirection={"column"}>
+      {loading ? <Spinner /> : <Box>User Profile</Box>}
+      <Flex flexDirection={"column"} alignItems={"center"}>
+        <Text fontSize={18}>username : {userData?.username}</Text>
+        <Button bgColor="green.400" color="white">Change name</Button>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default UserPage;
