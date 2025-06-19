0271704134f5253c4886cb941551c9e4316f5c23
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-19 11:12:25 +0200

    Updated home page with AllPostsList

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,13 +1,15 @@
-import { Image } from "@chakra-ui/react";
+import { Image, Box, Heading } from "@chakra-ui/react";
 import React from "react";
 import meme from "../images/27w39b.jpg";
+import AllPostsList from "../components/AllPostsList";
 
 const Homepage = () => {
   return (
-    <div>
-      Homepage
-      <Image src={meme} width={500} height={500} alt="meme" />
-    </div>
+    <Box textAlign="center" mt={6}>
+      <Heading size="lg">Homepage</Heading>
+      <Image src={meme} width={500} height={500} alt="meme" mx="auto" my={4} />
+      <AllPostsList />
+    </Box>
   );
 };
 
